2a859fcf5349a62e1e8a2115c95f3a129a0baa27
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-03-21 18:41:46 +0100

    Праця

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const imageList = document.querySelector(".gallery");
+let imageHTML = "";
+galleryItems.forEach((item) => {
+  let image = `
+  <li class="gallery__item">
+<a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="Image description"
+    />
+  </a></li>`;
+  imageHTML += image;
+});
+imageList.innerHTML = imageHTML;
+
+imageList.addEventListener("click", onImageClick);
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" >
+  `);
+  instance.show();
+  document.addEventListener("keydown", closeOnEsc);
+  function closeOnEsc(event) {
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+    document.removeEventListener("keydown", closeOnEsc);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const imageList = document.querySelector(".gallery");
+let imageHTML = "";
+galleryItems.forEach((item) => {
+  let image = `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="Image description"
+      />
+    </a>
+  </li>`;
+
+  imageHTML += image;
+});
+imageList.innerHTML = imageHTML;
+
+imageList.addEventListener("click", onImageClick);
+let galery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  galery.on("closed.simplelightbox", function () {
+    galery.refresh();
+  });
+}
